10fb1c0fb500c825d7196247ad1aeedd8e4ad938
Lucas Antunes de Freitas <lucas-freitas@MacBook-Pro-de-Lucas.local>
2024-12-12 23:13:36 -0300

    first commit

---
+++ app/sections/contact.tsx
@@ -0,0 +1,109 @@
+
+const Contact = () => {
+    return (
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                />
+            </div>
+            <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-balance text-4xl font-semibold tracking-tight text-blue-900 sm:text-5xl">Entre em contato conosco</h2>
+                <p className="mt-2 text-lg/8 text-gray-600">Preenche o formulário abaixo e saiba mais sobre o nosso produto.</p>
+            </div>
+            <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-16">
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <div className="sm:col-span-2">
+                        <label htmlFor="first-name" className="block text-sm/6 font-semibold text-blue-900">
+                            Informe seu nome
+                        </label>
+                        <div className="mt-2.5">
+                            <input
+                                id="first-name"
+                                name="first-name"
+                                type="text"
+                                autoComplete="given-name"
+                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="company" className="block text-sm/6 font-semibold text-blue-900">
+                            Empresa
+                        </label>
+                        <div className="mt-2.5">
+                            <input
+                                id="company"
+                                name="company"
+                                type="text"
+                                autoComplete="organization"
+                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm/6 font-semibold text-blue-900">
+                            E-mail
+                        </label>
+                        <div className="mt-2.5">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-blue-900">
+                            Telefone
+                        </label>
+                        <div className="mt-2.5">
+                            <div className="flex rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                                <input
+                                    id="phone-number"
+                                    name="phone-number"
+                                    type="text"
+                                    placeholder="123-456-7890"
+                                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="message" className="block text-sm/6 font-semibold text-blue-900">
+                            Informe sua mensagem
+                        </label>
+                        <div className="mt-2.5">
+                            <textarea
+                                id="message"
+                                name="message"
+                                rows={4}
+                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                defaultValue={''}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10">
+                    <button
+                        type="submit"
+                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Enviar
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ app/sections/features.tsx
@@ -0,0 +1,52 @@
+import { CircleStackIcon, MapPinIcon, PresentationChartBarIcon, WifiIcon } from '@heroicons/react/24/outline'
+
+
+const Features = () => {
+
+    const features = [
+        {
+            name: 'Acompanhe em tempo real',
+            description:
+                'Visualize em tempo real o roteiro de seus promotores e suas atividades agendadas no dia.',
+            icon: MapPinIcon,
+        },
+        {
+            name: 'Suas atividades off-line',
+            description:
+                'Seu promotor pode realizar suas tarefas básicas sem precisar estar conectado a internet.',
+            icon: WifiIcon,
+        },
+        {
+            name: 'Armazenamento de imagens',
+            description:
+                'Armazene as fotos tiradas pelo promotor para ter controle de suas atividades.',
+            icon: CircleStackIcon,
+        },
+        {
+            name: 'Relatório de desempenho',
+            description:
+                'Relatórios detalhados sobre o desempenho de seus promotores.',
+            icon: PresentationChartBarIcon,
+        },
+    ]
+
+    return (
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                {features.map((feature) => (
+                    <div key={feature.name} className="relative pl-16">
+                        <dt className="text-base/7 font-semibold text-blue-900">
+                            <div className="absolute left-0 top-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                                <feature.icon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            {feature.name}
+                        </dt>
+                        <dd className="mt-2 text-base/7 text-gray-600">{feature.description}</dd>
+                    </div>
+                ))}
+            </dl>
+        </div>
+    )
+}
+
+export default Features;
