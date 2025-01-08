38df9088a791a5531209129fc39b6dca02df980d
Lucas Antunes de Freitas <lucasrgs17@gmail.com>
2025-01-07 22:04:12 -0300

    change email to receive

--- app/service/send-mail.ts
+++ app/service/send-mail.ts
@@ -5,8 +5,8 @@ const sendEmail = async (emailFrom: string, message: string, name: string, compa
     const resend = new Resend(process.env.resendKey);
 
     return await resend.emails.send({
-        from: 'promotoragil@landingpage.dev',
-        to: 'contato@promotoragil.com.br',
+        from: (process.env.emailFrom as string),
+        to: (process.env.emailTo as string),
         subject: 'Contato via landing page',
         html: `
             <strong> E-mail: </strong> <p> ${emailFrom} <p>
